b028f47e2ea98dd6f25db42e4d043bc831898ac7
wrecktenwald <wrecktenwald@utexas.edu>
2023-04-11 15:27:50 -0500

    Adding additional airports data

--- test_noaa.py
+++ test_noaa.py
@@ -6,10 +6,22 @@ import pandas as pd
 from noaa import climate_data_etl
 
 
-kaus = climate_data_etl(path=f"{__file__.split('CS-391L-Final-Project')[0]}/CS-391L-Final-Project/data/noaa/3297764.csv")
-kiah = climate_data_etl(path=f"{__file__.split('CS-391L-Final-Project')[0]}/CS-391L-Final-Project/data/noaa/3297770.csv")
+base_path = f"{__file__.split('CS-391L-Final-Project')[0]}/CS-391L-Final-Project/data/noaa/"
+
+kaus = climate_data_etl(path=f"{base_path}3297764.csv")
+kiah = climate_data_etl(path=f"{base_path}3297770.csv")
+kdfw = climate_data_etl(path=f"{base_path}3298637.csv")
+ksat = climate_data_etl(path=f"{base_path}3298642.csv")
+khrl = climate_data_etl(path=f"{base_path}3298648.csv")
+kmaf = climate_data_etl(path=f"{base_path}3298651.csv")
+kama = climate_data_etl(path=f"{base_path}3298653.csv")
 
 tx_data = pd.concat([
     kaus, 
-    kiah
+    kiah,
+    kdfw,
+    ksat,
+    khrl,
+    kmaf, 
+    kama 
 ])
