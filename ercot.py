53770e5fea034088149e2b336bc116f730086f1c
Chris Lawson <calawson@utexas.edu>
2023-04-12 20:29:18 -0500

    Updated

--- ercot.py
+++ ercot.py
@@ -5,6 +5,11 @@ Load ERCOT load data from .csv files in the repository
 import numpy as np
 import pandas as pd
 
+def round_down(item):
+    idx = item.find("24:00")
+    if idx >= 0:
+        item = item[:idx] + "23:59"
+    return item
 
 def ercot_data(
     path    
@@ -23,13 +28,15 @@ def ercot_data(
         cleaner ERCOT data
     """
 
-    raw_data = pd.read_excel(filepath_or_buffer=path)
+    df = pd.read_excel(path)
     
     # Removing the ERCOT value as it is just the sum
     df = df[df.columns[:-1]]
-    df.head()
-    
-    df["DATE"] =  pd.to_datetime(df["Hour_End"])
+    if "Hour_End" in df.columns:
+        df["DATE"] = pd.to_datetime(df["Hour_End"])
+    elif "HourEnding" in df.columns:
+        df["HourEnding"] = df["HourEnding"].apply(round_down)
+        df["DATE"] = pd.to_datetime(df["HourEnding"])
     df["YEAR"] = [_.year for _ in df["DATE"]]
     df["MONTH"] = [_.month for _ in df["DATE"]]
     df["DAY"] = [_.day for _ in df["DATE"]]
