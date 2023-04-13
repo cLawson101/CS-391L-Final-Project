47d4bf6f688681f61a528e23fcbe8187fb1a99da
wrecktenwald <wrecktenwald@utexas.edu>
2023-04-12 20:08:53 -0500

    Adjusting noaa file

--- test_noaa.py
+++ test_noaa.py
@@ -6,15 +6,15 @@ import pandas as pd
 from noaa import climate_data_etl
 
 
-base_path = f"{__file__.split('CS-391L-Final-Project')[0]}/CS-391L-Final-Project/data/noaa/"
+noaa_base_path = f"{__file__.split('CS-391L-Final-Project')[0]}/CS-391L-Final-Project/data/noaa/"
 
-kaus = climate_data_etl(path=f"{base_path}3297764.csv")
-kiah = climate_data_etl(path=f"{base_path}3297770.csv")
-kdfw = climate_data_etl(path=f"{base_path}3298637.csv")
-ksat = climate_data_etl(path=f"{base_path}3298642.csv")
-khrl = climate_data_etl(path=f"{base_path}3298648.csv")
-kmaf = climate_data_etl(path=f"{base_path}3298651.csv")
-kama = climate_data_etl(path=f"{base_path}3298653.csv")
+kaus = climate_data_etl(path=f"{noaa_base_path}3297764.csv")
+kiah = climate_data_etl(path=f"{noaa_base_path}3297770.csv")
+kdfw = climate_data_etl(path=f"{noaa_base_path}3298637.csv")
+ksat = climate_data_etl(path=f"{noaa_base_path}3298642.csv")
+khrl = climate_data_etl(path=f"{noaa_base_path}3298648.csv")
+kmaf = climate_data_etl(path=f"{noaa_base_path}3298651.csv")
+kama = climate_data_etl(path=f"{noaa_base_path}3298653.csv")
 
 tx_data = pd.concat([
     kaus, 
