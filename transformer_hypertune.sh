8a69e6a83f365c23630bfe9a633b7379b145d4d7
Chris Lawson <calawson@utexas.edu>
2023-04-27 16:44:29 -0500

    One last touch to hypertuning

--- transformer_hypertune.sh
+++ transformer_hypertune.sh
@@ -3,15 +3,18 @@ for e in 10 20
 do
     for lr in 1e-3 1e-4
     do
-        for nl in 1 4
+        for nl in 1 4 8
         do
-            for tl in 24 48 
+            for d in 0.0 0.1 0.2
             do
-                for fw in 24 48
+                for tl in 24 48 
                 do
-                    for bs in 1 128
+                    for fw in 24 48
                     do
-                        python south_centra_wz_transformer_tacc.py --max_epochs ${e} --lr ${lr} --num_layers ${nl}  --training_length ${tl} --forecast_window ${fw} --batch_size ${bs} >> transformer_hypertuning.csv
+                        for bs in 1 128
+                        do
+                            python south_centra_wz_transformer_tacc.py --max_epochs ${e} --lr ${lr} --num_layers ${nl}  --training_length ${tl} --forecast_window ${fw} --batch_size ${bs} --dropout ${d} >> transformer_hypertuning.csv
+                        done
                     done
                 done
             done
